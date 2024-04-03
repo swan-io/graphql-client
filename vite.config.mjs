3c101d273c8447c6a43eeed6e53c5db1cc49a60b
bloodyowl <bloodyowl@icloud.com>
2024-04-03 09:38:01 +0200

    Remove unused config

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,15 +3,6 @@ import { loadEnv } from "vite";
 const env = loadEnv("development", process.cwd());
 
 export default {
-  server: {
-    proxy: {
-      "/api": {
-        target: env.VITE_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/\/api$/, ""),
-      },
-    },
-  },
   test: {
     environment: "jsdom",
     include: ["**/*.{test,spec}.ts"],
